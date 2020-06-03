ad30d7887f6bf5efb8151b46da63d14801d0f836
Valentyn <valentyn28101993@ukr.net>
2020-06-03 20:00:19 +0300

    redux-tutorial-todo

---
+++ redux-tutorial-todo/src/redux/types.js
@@ -0,0 +1,11 @@
+const Type = {
+  ADD_ТОDO: "ADD_TODO",
+  SET_VISIBILITY_FILTER: "SET_VISIBILITY_FILTER",
+  TOGGLE_ТОDO: "TOGGLE_TODO",
+  //   VisibilityFilters: {
+  //     SHOW_ALL: "SHOW_ALL",
+  //     SHOW_COMPLETED: "SHOW_COMPLETED",
+  //     SHOW_ACTIVE: "SHOW_ACTIVE",
+  //   },
+};
+export default Type;
